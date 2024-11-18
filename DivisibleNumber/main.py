023bb2b89a1a58ffb6bee5f4f2971d107db0339e
Trekker687 <113757673+Trekker687@users.noreply.github.com>
2024-11-18 17:41:21 +0530

    Add files via upload

---
+++ DivisibleNumber/main.py
@@ -0,0 +1,7 @@
+numerator = int(input("Enter the numerator "))
+denominator = int(input("Enter the denominator "))
+
+if numerator%denominator == 0:
+    print (numerator, "is divisible by" ,denominator)
+else:
+    print (numerator, "is not divisible by" ,denominator)
